2f589b684f2516f4fb68e69e564dbd3468ddc6eb
essex999 <oktav2222@gmail.com>
2024-04-08 00:16:03 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import react from '@vitejs/plugin-react'
-import sass from 'sass'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [react()],
-	css: {
-		preprocessorOptions: {
-			scss: {
-				implementation: sass,
-			},
-		},
-	},
+ 
 })
