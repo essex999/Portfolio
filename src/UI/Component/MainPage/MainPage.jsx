0d05b335c65f7ccf2f9526bf29ab46fc3a284c46
Essex X <120477833+essex999@users.noreply.github.com>
2024-04-08 15:58:03 +0000

    setUpContainer

--- src/UI/Component/MainPage/MainPage.jsx
+++ src/UI/Component/MainPage/MainPage.jsx
@@ -1,24 +1,24 @@
-import catImage from '../../../assets/neva-masquerade-royalty-free-image-1674509896.jpg'
-import styles from './MainPage.module.scss'
-import { NavBar } from './NavBar/NavBar'
+import catImage from "../../../assets/neva-masquerade-royalty-free-image-1674509896.jpg";
+import styles from "./MainPage.module.scss";
+import { NavBar } from "./NavBar/NavBar";
 export const MainPage = ({ childComponent: ChildComponent }) => {
-	const navButtons = ['About me', 'Resume', 'Portfolio', 'Blog', 'Contact']
+  const navButtons = ["About me", "Resume", "Portfolio", "Blog", "Contact"];
 
-	return (
-		<div className={styles.main}>
-			<div className={styles.header}>
-				<div className={styles.basicUserInfo}>
-					<h1 className={styles.textWrap}>Ceban Octavian</h1>
-					<h2>Web Developer</h2>
-				</div>
-				<img className={styles.img} src={catImage} />
-			</div>
-			<div className={styles.page}>
-				<NavBar navButtons={navButtons} />
-				<main className={styles.mainContainer}>
-					<ChildComponent />
-				</main>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <div className={styles.basicUserInfo}>
+          <h1 className={styles.textWrap}>Cxxx</h1>
+          <h2>Web Developer</h2>
+        </div>
+        <img className={styles.img} src={catImage} />
+      </div>
+      <div className={styles.page}>
+        <NavBar navButtons={navButtons} />
+        <main className={styles.mainContainer}>
+          <ChildComponent />
+        </main>
+      </div>
+    </div>
+  );
+};
