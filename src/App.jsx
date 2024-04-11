636bdc22ae37dd57e2fe50e0714425d2347cc581
essex999 <oktav2222@gmail.com>
2024-04-11 23:19:47 +0300

    finalScrollFix?

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
-import catImage from "./assets/neva-masquerade-royalty-free-image-1674509896.jpg";
-import styles from "./App.module.scss";
-import { NavBar } from "./UI/Component/NavBar/NavBar";
+import { useState } from 'react'
+import styles from './App.module.scss'
+import { NavBar } from './UI/Component/NavBar/NavBar'
+import catImage from './assets/neva-masquerade-royalty-free-image-1674509896.jpg'
 
 export const App = ({ childComponent: ChildComponent }) => {
-  const navButtons = ["About me", "Resume", "Portfolio", "Contact"];
+	const navButtons = ['About me', 'Resume', 'Portfolio', 'Contact']
+	const [isScrollToBot, setIsScrollToBot] = useState(false)
 
-  return (
-    <div className={styles.main}>
-      <div className={styles.header}>
-        <div className={styles.basicUserInfo}>
-          <h1 className={styles.text}>Cxxx</h1>
-          <h2 className={styles.text}>Web Developer</h2>
-        </div>
-        <img className={styles.img} src={catImage} />
-      </div>
-      <div className={styles.page}>
-        <NavBar navButtons={navButtons} />
-        <main className={styles.mainContainer}>
-          <ChildComponent />
-        </main>
-      </div>
-    </div>
-  );
-};
+	const handleSetDisplayHeader = cords => {
+		if (window.innerWidth < 769) {
+			cords === 0 ? setIsScrollToBot(false) : setIsScrollToBot(true)
+		} else return
+	}
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.main}>
+				<div
+					style={isScrollToBot ? { display: 'none' } : { display: 'flex' }}
+					className={styles.header}
+				>
+					<div className={styles.basicUserInfo}>
+						<h1 className={styles.text}>Cxxx</h1>
+						<h2 className={styles.text}>Web Developer</h2>
+					</div>
+					<img className={styles.img} src={catImage} />
+				</div>
+				<div className={styles.page}>
+					<NavBar navButtons={navButtons} />
+					<main
+						onScroll={e => {
+							handleSetDisplayHeader(e.target.scrollTop)
+						}}
+						className={styles.mainContainer}
+					>
+						<ChildComponent />
+					</main>
+				</div>
+			</div>
+		</div>
+	)
+}
