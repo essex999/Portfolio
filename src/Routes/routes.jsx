4b25ec97e4797af0bcac96330685244383d19650
Essex X <120477833+essex999@users.noreply.github.com>
2024-04-16 09:32:22 +0000

    fixRoutes4Gh-pages

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,28 +1,29 @@
-import { createBrowserRouter } from 'react-router-dom'
-import { App } from '../App'
-import { AboutMe } from '../UI/Screens/AboutMe/AboutMe'
-import { Contact } from '../UI/Screens/Contact/Contact'
-import { Portfolio } from '../UI/Screens/Portfolio/Portfolio'
-import { Resume } from '../UI/Screens/Resume/Resume'
+import { createBrowserRouter } from "react-router-dom";
+import { App } from "../App";
+import { AboutMe } from "../UI/Screens/AboutMe/AboutMe";
+import { Contact } from "../UI/Screens/Contact/Contact";
+import { Portfolio } from "../UI/Screens/Portfolio/Portfolio";
+import { Resume } from "../UI/Screens/Resume/Resume";
+import { basePatchPrefix } from "../const/const";
 export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App childComponent={AboutMe} />,
-	},
-	{
-		path: '/aboutme',
-		element: <App childComponent={AboutMe} />,
-	},
-	{
-		path: '/resume',
-		element: <App childComponent={Resume} />,
-	},
-	{
-		path: '/portfolio',
-		element: <App childComponent={Portfolio} />,
-	},
-	{
-		path: '/contact',
-		element: <App childComponent={Contact} />,
-	},
-])
+  {
+    path: `${basePatchPrefix}`,
+    element: <App childComponent={AboutMe} />,
+  },
+  {
+    path: `${basePatchPrefix}aboutme`,
+    element: <App childComponent={AboutMe} />,
+  },
+  {
+    path: `${basePatchPrefix}resume`,
+    element: <App childComponent={Resume} />,
+  },
+  {
+    path: `${basePatchPrefix}portfolio`,
+    element: <App childComponent={Portfolio} />,
+  },
+  {
+    path: `${basePatchPrefix}contact`,
+    element: <App childComponent={Contact} />,
+  },
+]);
--- src/UI/ReusedComponents/NavButton/NavButton.jsx
+++ src/UI/ReusedComponents/NavButton/NavButton.jsx
@@ -1,75 +1,79 @@
-import { useEffect, useRef, useState } from 'react'
-import { useHref, useNavigate } from 'react-router-dom'
-import styles from './NavButton.module.scss'
+import { useEffect, useRef, useState } from "react";
+import { useHref, useNavigate } from "react-router-dom";
+import styles from "./NavButton.module.scss";
+import { basePatchPrefix } from "../../../const/const";
 
 export const NavButton = ({
-	textContent,
-	setSelectedButton,
-	selectedButton,
+  textContent,
+  setSelectedButton,
+  selectedButton,
 }) => {
-	const buttonRef = useRef(null)
-	const path = useHref()
+  const buttonRef = useRef(null);
+  const path = useHref();
+  console.log(path);
+  const [isHover, setIsHover] = useState(false);
+  const [currentTextContent, setcurrentTextContent] = useState("");
 
-	const [isHover, setIsHover] = useState(false)
-	const [currentTextContent, setcurrentTextContent] = useState('')
+  const convertToParam = (str) => {
+    return str.split(" ").join("").toLowerCase();
+  };
 
-	const convertToParam = str => {
-		return str.split(' ').join('').toLowerCase()
-	}
+  const getPath = () => {
+    if (path === "/aboutme") {
+      return "About me";
+    }
+    return path
+      .split("")
+      .map((el, index) => {
+        if (el === "/") {
+          return;
+        } else if (index === 1) {
+          return el.toUpperCase();
+        } else return el;
+      })
+      .join("");
+  };
 
-	const getPath = () => {
-		if (path === '/aboutme') {
-			return 'About me'
-		}
-		return path
-			.split('')
-			.map((el, index) => {
-				if (el === '/') {
-					return
-				} else if (index === 1) {
-					return el.toUpperCase()
-				} else return el
-			})
-			.join('')
-	}
+  useEffect(() => {
+    setcurrentTextContent(buttonRef.current.textContent);
+    setSelectedButton(getPath());
+    console.log(getPath());
+  }, []);
+  const navigate = useNavigate();
+  return (
+    <form
+      ref={buttonRef}
+      onSubmit={(e) => {
+        e.preventDefault();
+        setcurrentTextContent(e.target.textContent);
+        setSelectedButton(e.target.textContent);
+        console.log(
+          `${basePatchPrefix}${convertToParam(e.target.textContent)}`,
+        );
+        navigate(`${basePatchPrefix}${convertToParam(e.target.textContent)}`);
+      }}
+      onMouseEnter={() => {
+        setIsHover(true);
+      }}
+      onMouseLeave={() => {
+        setIsHover(false);
+      }}
+      className={styles.navButtonGroup}
+    >
+      <button className={styles.navButton}>{textContent}</button>
 
-	useEffect(() => {
-		setcurrentTextContent(buttonRef.current.textContent)
-		setSelectedButton(getPath())
-		console.log(getPath())
-	}, [])
-	const navigate = useNavigate()
-	return (
-		<form
-			ref={buttonRef}
-			onSubmit={e => {
-				e.preventDefault()
-				setcurrentTextContent(e.target.textContent)
-				setSelectedButton(e.target.textContent)
-				navigate(`/${convertToParam(e.target.textContent)}`)
-			}}
-			onMouseEnter={() => {
-				setIsHover(true)
-			}}
-			onMouseLeave={() => {
-				setIsHover(false)
-			}}
-			className={styles.navButtonGroup}
-		>
-			<button className={styles.navButton}>{textContent}</button>
+      <div
+        onClick={() => {
+          console.log(buttonRef.current.textContent);
 
-			<div
-				onClick={() => {
-					console.log(buttonRef.current.textContent)
-
-					setcurrentTextContent(buttonRef.current.textContent)
-					setSelectedButton(buttonRef.current.textContent)
-					navigate(`/${convertToParam(buttonRef.current.textContent)}`)
-				}}
-				className={`${styles.underline} ${
-					isHover && selectedButton !== currentTextContent ? styles.hover : ''
-				} ${selectedButton === currentTextContent ? styles.clicked : ''}`}
-			/>
-		</form>
-	)
-}
+          setcurrentTextContent(buttonRef.current.textContent);
+          setSelectedButton(buttonRef.current.textContent);
+          navigate(`/${convertToParam(buttonRef.current.textContent)}`);
+        }}
+        className={`${styles.underline} ${
+          isHover && selectedButton !== currentTextContent ? styles.hover : ""
+        } ${selectedButton === currentTextContent ? styles.clicked : ""}`}
+      />
+    </form>
+  );
+};
