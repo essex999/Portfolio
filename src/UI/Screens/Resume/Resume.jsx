d691cd3e1334b6767b2dc5c92d4fed5e31213b0c
Essex X <120477833+essex999@users.noreply.github.com>
2024-04-18 14:00:44 +0000

    commit

--- src/UI/Screens/Resume/Resume.jsx
+++ src/UI/Screens/Resume/Resume.jsx
@@ -1,7 +1,23 @@
+import styles from "./Resume.module.scss";
+
 export const Resume = () => {
-	return (
-		<>
-			<div>Resume</div>
-		</>
-	)
-}
+  return (
+    <>
+      <div className={styles.textContainer}>
+        <div className={styles.sticker}>
+          <p className={styles.stickerText}>Education</p>
+        </div>
+        <p className={styles.textContent}>
+          College of Microelectronics and Computerized Engineering 2013-2017
+        </p>
+        <p className={styles.textContent}>Speciality: Telecommunications</p>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.sticker}>
+          <p className={styles.stickerText}>Work Experience</p>
+        </div>
+        <p className={styles.textContent}>{"aboutMeTextContent"}</p>
+      </div>
+    </>
+  );
+};
